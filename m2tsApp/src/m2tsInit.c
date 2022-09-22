89f0d13688131cacba0a877bf25c8fbc3361fde1
Matt Rippa <matt.rippa@noirlab.edu>
2022-09-21 18:34:32 -1000

    Fixed typo in function name (#17)

--- m2tsApp/src/m2tsInit.c
+++ m2tsApp/src/m2tsInit.c
@@ -48,5 +48,26 @@ static void initM2TSRegister(void) {
     iocshRegister(&initM2TSFuncDef, initM2TSCallFunc);
 }
 
-epicsExportRegistrar(initM2TSRegister);
+/*M2ReadStatAP323*/
+static const iocshFuncDef M2ReadStatAP323FuncDef = {"M2ReadStatAP323", 0, NULL};
+static void M2ReadStatAP323Func(const iocshArgBuf *args) {
+    M2ReadStatAP323();
+}
 
+static void M2ReadStatAP323Register(void) {
+    iocshRegister(&M2ReadStatAP323FuncDef, M2ReadStatAP323Func);
+}
+
+/*M2AcqAP323*/
+static const iocshFuncDef M2AcqAP323FuncDef = {"M2AcqAP323", 0, NULL};
+static void M2AcqAP323Func(const iocshArgBuf *args) {
+    M2AcqAP323();
+}
+
+static void M2AcqAP323Register(void) {
+    iocshRegister(&M2AcqAP323FuncDef, M2AcqAP323Func);
+}
+
+epicsExportRegistrar(initM2TSRegister);
+epicsExportRegistrar(M2ReadStatAP323Register);
+epicsExportRegistrar(M2AcqAP323Register);
