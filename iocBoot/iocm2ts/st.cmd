c9b3a3055cf859ed7e0acc87c298d715aef7d325
Matt Rippa <matt.rippa@noirlab.edu>
2022-09-23 07:55:08 -1000

    Basic setup for devAP323 prng support.

--- iocBoot/iocm2ts/st.cmd
+++ iocBoot/iocm2ts/st.cmd
@@ -15,11 +15,6 @@ m2ts_registerRecordDeviceDriver pdbbase
 dbLoadTemplate "db/user.substitutions"
 dbLoadRecords "db/m2tsVersion.db", "user=mrippa"
 dbLoadRecords "db/dbSubExample.db", "user=mrippa"
-#dbLoadRecords("db/AP323.db","P=test:ap323data,D=Random,S=324235")
-#dbLoadRecords("db/AP323.db","P=test:ap323dataA,D=RandomAsync,S=324235")
-#dbLoadRecords("db/AP323.db","P=test:ap323dataI,D=RandomIntr,S=324235")
-dbLoadRecords("db/AP323.db","P=test:ap323dataI2,D=RandomIntr,S=324235,SCAN=I/O Intr")
-dbLoadRecords("db/AP323.db","P=test:ap323dataI3,D=RandomIntr,S=324235,SCAN=I/O Intr")
 
 #- Set this to see messages from mySub
 #var mySubDebug 1
