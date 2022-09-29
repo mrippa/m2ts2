8384565b4946a55836c0dbad471c520a016e4cbb
Matt Rippa <matt.rippa@noirlab.edu>
2022-09-28 15:42:23 -1000

    Implement scanIoInterrupt version of AP323.

--- iocBoot/iocm2ts/st.cmd
+++ iocBoot/iocm2ts/st.cmd
@@ -16,7 +16,10 @@ dbLoadTemplate "db/user.substitutions"
 dbLoadRecords "db/m2tsVersion.db", "user=mrippa"
 dbLoadRecords "db/dbSubExample.db", "user=mrippa"
 #dbLoadRecords("db/AP323.db","P=test:ap323data,D=Random,S=324235")
-dbLoadRecords("db/AP323.db","P=test:ap323dataA,D=RandomAsync,S=324235")
+#dbLoadRecords("db/AP323.db","P=test:ap323dataA,D=RandomAsync,S=324235")
+#dbLoadRecords("db/AP323.db","P=test:ap323dataI,D=RandomIntr,S=324235")
+dbLoadRecords("db/AP323.db","P=test:ap323dataI2,D=RandomIntr,S=324235,SCAN=I/O Intr")
+dbLoadRecords("db/AP323.db","P=test:ap323dataI3,D=RandomIntr,S=324235,SCAN=I/O Intr")
 
 #- Set this to see messages from mySub
 #var mySubDebug 1
