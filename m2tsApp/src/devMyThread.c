42226da91575021eeae26d6eac046ee8b0cdd244
Matt Rippa <matt.rippa@noirlab.edu>
2023-03-07 16:22:09 -1000

    Backed out some horriblie test code.

--- m2tsApp/src/devMyThread.c
+++ m2tsApp/src/devMyThread.c
@@ -25,19 +25,16 @@
     } while (0)
 
 
-static void M2AcqAP323_show(int channel_number)
+void M2AcqAP323_show()
 {
 
     if (!c_block323.bInitialized)
         printf("\n>>> ERROR: BOARD ADDRESS NOT SET <<<\n");
     else
     {
-        for (i = 0; i <= channel_number; i++)
+        for (i = 0; i < 101; i++)
         {
-            for (i = 0; i < 101; i++)
-            {
-                printf("%12.6f\n", ((((double)c_block323.s_cor_buf[0][i]) * 20.0) / (double)65536.0) + (-10.0));
-            }
+            printf("%12.6f\n", ((((double)c_block323.s_cor_buf[0][i]) * 20.0) / (double)65536.0) + (-10.0));
         }
     }
 }
@@ -84,110 +81,8 @@ static void* M2AcqAP323_runOnce()
 
 int M2AcqStart() {
 
-    int i = 0;
-
-    for (i=0; i<50; i++){
-        M2AcqAP323_runOnce();
-        M2AcqAP323_show(0);
-    }
+    M2AcqAP323_run();
     printf("M2AcqStart finished\n");
 
     return(0);
 }
-
-int tsetup()
-{
-    int s, tnum, num_threads;
-    struct thread_info *tinfo;
-    pthread_attr_t attr;
-    int stack_size;
-    void *res;
-
-    int argc = 3;
-    char *argv[] = {"t0", "t1", "t2"};
-
-    /* The "-s" option specifies a stack size for our threads */
-
-    stack_size = -1;
-
-    num_threads = argc - optind;
-    num_threads += 1; /* Add one more thread for m2 acquisition*/
-
-    /* Initialize thread creation attributes */
-    s = pthread_attr_init(&attr);
-    if (s != 0)
-        handle_error_en(s, "pthread_attr_init");
-
-    if (stack_size > 0)
-    {
-        s = pthread_attr_setstacksize(&attr, stack_size);
-        if (s != 0)
-            handle_error_en(s, "pthread_attr_setstacksize");
-    }
-
-    /* Allocate memory for pthread_create() arguments */
-
-    tinfo = calloc(num_threads, sizeof(struct thread_info));
-    if (tinfo == NULL)
-        handle_error("calloc");
-
-    /* Create one thread for each command-line argument */
-
-    for (tnum = 0; tnum < num_threads; tnum++)
-    {
-        tinfo[tnum].thread_num = tnum + 1;
-        tinfo[tnum].argv_string = argv[optind + tnum];
-
-        /* The pthread_create() call stores the thread ID into
-           corresponding element of tinfo[] */
-
-        s = pthread_create(&tinfo[tnum].thread_id, &attr,
-                           &thread_start, &tinfo[tnum]);
-        if (s != 0)
-            handle_error_en(s, "pthread_create");
-    }
-
-    /*Create a final thread for acquisiton. tnum is the last thread*/
-    s = pthread_create(&tinfo[tnum].thread_id, &attr, &M2AcqAP323_run, NULL);
-
-    if (s != 0)
-        handle_error_en(s, "pthread_create m2Acq");
-
-    setM2AcqTInfo(&tinfo[tnum]);
-
-    /* Destroy the thread attributes object, since it is no
-       longer needed */
-    s = pthread_attr_destroy(&attr);
-    if (s != 0)
-        handle_error_en(s, "pthread_attr_destroy");
-
-    /* Now join with each thread, and display its returned value */
-
-    for (tnum = 0; tnum < num_threads - 1; tnum++) /* Don't start the final m2Acq thread yet*/
-    {
-
-        printf("Starting tnum... %d\n", tnum);
-        s = pthread_join(tinfo[tnum].thread_id, &res);
-        if (s != 0)
-            handle_error_en(s, "pthread_join");
-
-        printf("Joined with thread %d; returned value was %s\n",
-               tinfo[tnum].thread_num, (char *)res);
-        free(res); /* Free memory allocated by thread */
-    }
-
-    free(tinfo);
-
-    return 0;
-}
-
-#ifndef MATT_TEST
-int main()
-{
-
-    tsetup();
-    printf("Hello world\n");
-
-    exit(EXIT_SUCCESS);
-}
-#endif
