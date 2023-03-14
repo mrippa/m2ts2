8cd5746792c462f3707594fc820b98bd32063619
Matt Rippa <matt.rippa@noirlab.edu>
2023-03-14 09:09:29 -1000

    Test small refactor to clean up AP323 init code.

--- m2tsApp/src/m2tsInit.c
+++ m2tsApp/src/m2tsInit.c
@@ -48,38 +48,5 @@ static void initM2TSRegister(void) {
     iocshRegister(&initM2TSFuncDef, initM2TSCallFunc);
 }
 
-/*M2ReadStatAP323*/
-static const iocshFuncDef M2ReadStatAP323FuncDef = {"M2ReadStatAP323", 0, NULL};
-static void M2ReadStatAP323Func(const iocshArgBuf *args) {
-    M2ReadStatAP323();
-}
-
-static void M2ReadStatAP323Register(void) {
-    iocshRegister(&M2ReadStatAP323FuncDef, M2ReadStatAP323Func);
-}
-
-/*M2AcqAP323*/
-/* static const iocshFuncDef M2AcqAP323FuncDef = {"M2AcqAP323", 0, NULL};
-static void M2AcqAP323Func(const iocshArgBuf *args) {
-    M2AcqAP323();
-}
-
-static void M2AcqAP323Register(void) {
-    iocshRegister(&M2AcqAP323FuncDef, M2AcqAP323Func);
-}
- */
-
-/*M2AcqStart*/
-static const iocshFuncDef M2AcqStartFuncDef = {"M2AcqStart", 0, NULL};
-static void M2AcqStartFunc(const iocshArgBuf *args) {
-    M2AcqStart();
-}
-
-static void M2AcqStartRegister(void) {
-    iocshRegister(&M2AcqStartFuncDef, M2AcqStartFunc);
-}
-
 epicsExportRegistrar(initM2TSRegister);
-epicsExportRegistrar(M2ReadStatAP323Register);
-//epicsExportRegistrar(M2AcqAP323Register);
-epicsExportRegistrar(M2AcqStartRegister);
+
