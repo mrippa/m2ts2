38c39e02c40e5d4ff6dfa6d328a3966d75e13bda
Matt Rippa <matt.rippa@noirlab.edu>
2023-06-14 08:58:11 -1000

    Debug#2  M2TS_ShowAP471States

--- m2tsApp/src/ConfigAP471.c
+++ m2tsApp/src/ConfigAP471.c
@@ -15,6 +15,23 @@ int M2TSConfigAP471(int cardNumber)
         return (ERROR);
     }    
 
+    printf("\n\nConfiguration Parameters\n\n");
+    printf(" 1. Return to Previous Menu\n");
+    printf(" 2. Parameter Mask:    0x%02X\n", p471Card->c_block.param);
+    printf(" 3. Debounce Clock:    0x%02X\n", p471Card->c_block.deb_clock);
+    printf(" 4. Debounce Duration: R0 = 0x%08X R1 = 0x%08X R2 = 0x%08X\n",
+           p471Card->c_block.deb_duration[0], p471Card->c_block.deb_duration[1], p471Card->c_block.deb_duration[2]);
+    printf(" 5. Debounce Control:  R0 =     0x%04X R1 =     0x%04X R2 =     0x%04X\n",
+           p471Card->c_block.deb_control[0], p471Card->c_block.deb_control[1], p471Card->c_block.deb_control[2]);
+    printf(" 6. Write Mask:        R0 =     0x%04X R1 =     0x%04X R2 =     0x%04X\n",
+           p471Card->c_block.WriteMask[0], p471Card->c_block.WriteMask[1], p471Card->c_block.WriteMask[2]);
+    printf(" 7. Event Type:        R0 =     0x%04X R1 =     0x%04X R2 =     0x%04X\n",
+           p471Card->c_block.EventType[0], p471Card->c_block.EventType[1], p471Card->c_block.EventType[2]);
+    printf(" 8. Event Polarity:    R0 =     0x%04X R1 =     0x%04X R2 =     0x%04X\n",
+           p471Card->c_block.EventPolarity[0], p471Card->c_block.EventPolarity[1], p471Card->c_block.EventPolarity[2]);
+    printf(" 9. Event Enable:      R0 =     0x%04X R1 =     0x%04X R2 =     0x%04X\n",
+           p471Card->c_block.EventEnable[0], p471Card->c_block.EventEnable[1], p471Card->c_block.EventEnable[2]);
+
     printf("Config AP471 done! 0x%x\n", status);
 
     return status;
